cc4ee47d72dd44eb26e0f8e38d733b3a3fa97085
TheFearlessCheese <58084006+TheFearlessCheese@users.noreply.github.com>
2022-02-17 14:52:00 -0700

    loo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <algorithm>
 
-int binarySearch(std::vector<std::string> &dictionary, std::string query)
+void binarySearch(std::vector<std::string> &dictionary, std::string &query)
 {
     /*Lowercase Query*/
     std::transform(query.begin(), query.end(), query.begin(), ::tolower);
 
+    /*Binary Search for the Query*/
     size_t minIndex = 0;
     size_t maxIndex = dictionary.size();
     size_t midIndex;
@@ -16,10 +18,10 @@ int binarySearch(std::vector<std::string> &dictionary, std::string query)
     {
         midIndex = (minIndex + maxIndex) / 2;
 
-        //Check if query equals middle element
-        if (query == dictionary[midIndex])
+        if (query == dictionary[midIndex]) //Check if query equals middle element
         {
-            return midIndex;
+            /*Word was located*/
+            std::cout << "Word located in dictionary at " << midIndex << " !" << std::endl;
         }
         else if (query > dictionary[midIndex]) //check if query is greater than the mid element
         {
@@ -30,21 +32,71 @@ int binarySearch(std::vector<std::string> &dictionary, std::string query)
             maxIndex = midIndex - 1;
         }
     }
+
+    /*Word was not located*/
+    std::cout << "Could not locate word in dictionary!" << std::endl;
 }
 
 int main()
 {
+ 
 
+    /*Initilaize dictionary*/
+    std::vector<std::string> dictionary;
+    size_t wordNum = 0;
+    std::string line;
+    std::ifstream dictionaryFile("dictionary.txt");
 
-    
+    while (std::getline(dictionaryFile, line))
+    {
+        dictionary[wordNum++] = line;
+    }
+
+    std::cout << "LOADED " << wordNum << " words!"<< std::endl;
+
     /*Title*/
-    std::cout << "|----- Spell Checker -----|\n";
+    std::cout << "|----- Welcome to Spell Checker -----|\n";
 
     /*Main Menu Loop*/
     size_t choice;
+    std::string query;
 
     while (choice != 5)
     {
         std::cout << "\nMain Menu \n1: Spell Check a Word (Linear Search) \n2: Spell Check a Word (Binary Search) \n3: Spell Check Alice In Wonderland (Linear Search) \n4: Spell Check Alice In Wonderland (Binary Search) \n5: Exit \nEnter menu selection (1-5): ";
+        std::cin >> choice;
+
+        switch (choice)
+        {  
+            /*Spell Check a Word(Linear Search)*/
+            case 1:
+                break;
+ 
+            /*Spell Check a Word(Binary Search)*/
+            case 2:
+                std::cout << "Please enter a word: " << std::endl;
+                std::cin >> query;
+                std::cout << std::endl;
+                binarySearch(dictionary, query);
+                break;
+
+            /*Spell Check Alice In Wonderland(Linear Search)*/
+            case 3:
+                break;
+
+            /*Spell Check Alice In Wonderland(Binary Search)*/
+            case 4:
+                break;
+            
+            /*Exit*/
+            case 5:
+                std::cout << "ERROR: INVALID SELECTION!" << std::endl;
+                break;
+
+            default:
+                break;
+        }
     }
+
+    return 0;
 }
